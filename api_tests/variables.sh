50fae8fe1546dc500ff32478006a88b681b33223
coding-to-music <connors.tom@gmail.com>
2021-12-30 06:09:30 +0000

    build

--- api_tests/variables.sh
+++ api_tests/variables.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-worker_url="https://mongo-realm-worker.YOUR_SUBDOMAIN.workers.dev"
-api_key="YOUR_REALM_AUTH_API_KEY"
+worker_url="https://mongo-realm-worker.coding-to-music.workers.dev"
+api_key="prxhnSMIju04YfNwCt26FaqTTr6XkWnPexkBIC160bjcaab2BKOZyeCYE0ITcvQx"
